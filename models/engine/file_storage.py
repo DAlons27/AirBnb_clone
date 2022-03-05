3045888620c774b42923b7e05ef65447a4498264
DAlons27 <alonso.morey.arq@gmail.com>
2022-03-04 22:40:56 -0500

    Adding class FileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,44 +11,52 @@ import os
 import json
 
 class FileStorage:
-    """Class where we serialize and wish to serialize Json files"""
-    all_class = {"BaseModel": BaseModel, "User": User, "State": State, "City": City, "Review": Review, "Amenity": Amenity, "Place": Place}
-    __objets = {}
-    __file_path ='file.json'
-    
+    """A class for Serializing and Deserializing JSON files"""
+    __file_path = 'file.json'
+    __objects = {}
+    all_classes = {"BaseModel": BaseModel, "User": User, "State": State,
+                  "City": City, "Amenity": Amenity, "Place": Place,
+                  "Review": Review}
+
     def __init__(self):
-        """Initiation"""
+        """Default initializing"""
         pass
 
     def all(self):
-        """Returns the dictionary objects"""
-        return self.__objets
+        """Returns a dictionary with all the objects of the program"""
+        return self.__objects
+
+    def path(self):
+        """Returns the path of the JSON file"""
+        return self.__file_path
 
     def new(self, obj):
-        """"Create a new object"""
-        obj_key = "{}.{}".format(obj.__class__.__.name__, obj.id)
-        self.__objects[obj_key] = obj 
+        """Creates a new object for the program, or updates an older object"""
+        obj_key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[obj_key] = obj
 
     def save(self):
-        """Serialization"""
+        """Serialization:
+            Saves all dictionaries from the objects to 'file.json'
+        """
         dct = self.__objects.copy()
-        for i, j in dct.items():
-            dct[i] = j.to_dict()
+        for k, o in dct.items():
+            dct[k] = o.to_dict()
         with open(self.__file_path, 'w') as fp:
             json.dump(dct, fp)
-        
+
     def reload(self):
-        """Deserialization"""
+        """Deserialization:
+        If 'file.json' exists, extracts all its dicts and converts them in
+        objects to be read for the program
+        """
         try:
             if os.stat(self.__file_path).st_size == 0:
                 raise EOFError()
             with open(self.__file_path, 'r', encoding='UTF-8') as fp:
                 obj = json.load(fp)
+
         except FileNotFoundError:
             pass
         except EOFError:
             pass
-        
-    def path(self):
-        """Returns the path of the Json file"""
-        return self.__file_path
