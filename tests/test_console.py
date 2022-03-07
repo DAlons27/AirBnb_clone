1e90f2d2acce20d22ca6273ded19eed9f1e02969
edgar <edgar-william@hotmail.com>
2022-03-07 04:35:43 +0000

    test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -2,5 +2,15 @@
 """
 Test Console
 """
-import unittest
-import insperct
+import uuid
+import models
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.review import Review
+from models.amenity import Amenity
+from models.place import Place
+import os
+import json
+from datetime import datetime
